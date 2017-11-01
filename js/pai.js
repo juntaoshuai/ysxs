911a6cdbb78fff259b514874022dc98b1df78ae8
juntaoshuai <5955979939@qq.com>
2017-11-01 23:41:05 +0800

    paijs

---
+++ js/pai.js
@@ -0,0 +1,63 @@
+$(function(){
+    
+    // 出现删除小图标
+    $(".upload-list").on('click','img',function(){
+        if(!$(this).next(".del-photo").length){
+            $(this).parent().append("<span class='del-photo'>×</span>")
+        }
+    })
+    
+    //删除图片
+    $(".upload-list").on('click','.del-photo',function(){
+        var $this = $(this);
+        //询问框
+        layer.open({
+            title: '提示信息'
+            ,content: '您确定删除该图片吗？'
+            ,btn: ['确定','取消']
+            ,yes: function(index){
+                $this.parent().remove();
+                layer.close(index);
+            }
+        });
+        
+        
+    })
+
+    //toast使用jquery layer mobile 官网地址： http://layer.layui.com/mobile/
+    //封装layer方法
+    function myLayer(con) {
+        layer.open({
+            content: con,
+            skin: 'msg',
+            time: 2//2秒后自动关闭
+        });
+    
+    }
+    
+
+    //选择作品分类
+    $(".works-type").on('click',function(){
+        layer.open({
+            type: 1
+            ,content: ''
+            ,anim: 'up'
+            ,style: 'position:fixed; bottom:0; left:0; width: 100%; height: 200px; padding:10px 0; border:none;'
+        });    
+    })
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
