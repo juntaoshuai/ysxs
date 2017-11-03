391d0eed04849dc133db3b0be25df6671524e8f2
juntaoshuai <5955979939@qq.com>
2017-11-03 23:47:38 +0800

    upload

--- js/pai.js
+++ js/pai.js
@@ -1,27 +1,27 @@
-$(function(){
-    
+$(function () {
+
     // 出现删除小图标
-    $(".upload-list").on('click','img',function(){
-        if(!$(this).next(".del-photo").length){
+    $(".upload-list").on('click', 'img', function () {
+        if (!$(this).next(".del-photo").length) {
             $(this).parent().append("<span class='del-photo'>×</span>")
         }
     })
-    
+
     //删除图片
-    $(".upload-list").on('click','.del-photo',function(){
+    $(".upload-list").on('click', '.del-photo', function () {
         var $this = $(this);
         //询问框
         layer.open({
             title: '提示信息'
-            ,content: '您确定删除该图片吗？'
-            ,btn: ['确定','取消']
-            ,yes: function(index){
+            , content: '您确定删除该图片吗？'
+            , btn: ['确定', '取消']
+            , yes: function (index) {
                 $this.parent().remove();
                 layer.close(index);
             }
         });
-        
-        
+
+
     })
 
     //toast使用jquery layer mobile 官网地址： http://layer.layui.com/mobile/
@@ -32,14 +32,12 @@ $(function(){
             skin: 'msg',
             time: 2//2秒后自动关闭
         });
-    
+
     }
+
+
+
     
-
-
-    
-
-
 
 
 
